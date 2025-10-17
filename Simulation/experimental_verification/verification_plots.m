5cdf488821c31e2e2bdc4e809cc35270a1ea90d1
Cameron <cameronzorio@gmail.com>
2025-10-17 12:36:36 -0700

    finished experimental work

--- Simulation/experimental_verification/verification_plots.m
+++ Simulation/experimental_verification/verification_plots.m
@@ -1,5 +1,5 @@
 clc; clear
-load("hybrid_verification4.mat")
+load("passive_verification3.mat")
 % load("manual_balance_tumbling.mat")
 %{
 passive verification 1-3 contain tumbling data after all 10 passive runs,
@@ -49,13 +49,19 @@ for i = 1:numFrames % <---  REMEMBER TO INCLUDE/NOT INCLUDE RW WHEEL DATA HERE!!
 end
 
 
-t_start = 11;
+%% FIND GOOD STARTING POINT AND SAVE KE
+figure
+plot(t,eulerAngles)
+
+t_start = 14;
 start_idx = find(t > t_start, 1, 'first');
 t_0 = t(start_idx);
 
 t_plt = t(start_idx:end)-t_0;
 eulerAngles_plt = deg2rad(eulerAngles(start_idx:end,1:3));
 bodyRates_plt = bodyRates(start_idx:end,1:3);
+
+
 n = length(t_plt);
 KE = zeros(n,1);
 for i = 1:n
@@ -63,12 +69,17 @@ for i = 1:n
     KE(i) = (1/2)*omega'*J*omega;
 end
 
-%%
+save("passive_KE.mat","KE","t_plt")
+
+%% SAVE TORQUE
 vel_x = bodyRates_plt(:,1);
+vel_y = bodyRates_plt(:,2);
+vel_z = bodyRates_plt(:,3);
 
 % Design a FIR filter to differentiate --------------------------------
-figure
-pwelch(vel_x,[],[],[],sampleRate)
+% figure
+% pwelch(vel_x,[],[],[],sampleRate) 
+
 Fpass = 0.05;
 Fstop = 0.3;
 Fs = sampleRate;
@@ -80,42 +91,55 @@ d = designfilt('differentiatorfir', ...
     'SampleRate', Fs);
 
 accel_x = filter(d,vel_x)/sampleTime;
-delay = mean(grpdelay(d));
+accel_y = filter(d,vel_y)/sampleTime;
+accel_z = filter(d,vel_z)/sampleTime;
 
-t_d = t(1:end-delay);
+gd = round(mean(grpdelay(d)));     % constant group delay for linear-phase FIR
+t_al = t_plt(1:end-gd);
+vel_x_al   = vel_x(1:end-gd);
+vel_y_al   = vel_y(1:end-gd);
+vel_z_al   = vel_z(1:end-gd);
+accel_x_al = accel_x(1+gd:end);
+accel_y_al = accel_y(1+gd:end);
+accel_z_al = accel_z(1+gd:end);
 
-accel_x_d = accel_x;
-accel_x_d(1:delay) = [];
-
-t_d(1:delay) = [];
-accel_x_d(1:delay) = [];
-
-plot(t_plt,accel_x)
-xlabel('Time (s)')
-ylabel('Angular Acceleration [rad/s]')
+figure; hold on; grid on
 yline(0)
-xlim([t_d(1) t_d(end)])
-grid
+plot(t_al, vel_x_al);
+plot(t_al, accel_x_al);
+legend("velocity","acceleration"); 
+xlabel('Time (s)')
+title("pick good t_start for saving torque")
+
+torque = zeros(length(t_al),3);
+torque_norm = zeros(length(t_al),1);
+for i = 1:length(t_al)
+    omega_dot = [accel_x_al(i), accel_y_al(i), accel_z_al(i)]';
+    omega = [vel_x_al(i), vel_y_al(i), vel_z_al(i)]';
+    torque(i,:) = J*omega_dot + cross(omega,J*omega);
+    torque_norm(i) = norm(torque(i,:));
+end
+
+t_start = 5.4;
+start_idx = find(t_al > t_start, 1, 'first');
+t_0 = t_al(start_idx);
+
+t_al = t_al(start_idx:end)-t_0;
+torque = torque(start_idx:end,:);
+torque_norm = torque(start_idx:end);
+figure; hold on; grid on
+yline(0)
+plot(t_al, torque);
+t_plt = t(start_idx:end)-t_0;
+title("Torque")
+save("passive_torque.mat","t_al","torque_norm")
 
 
-%%
-figure
-plot(t,eulerAngles,'LineWidth',1.5);
-f = gen_single_fig();
-% plot(t_plt,bodyRates_plt,'LineWidth',1.5);
-plot(t_plt,KE,'LineWidth',1.5)
-grid on
-fontsize(13,'points')
-xlabel("Time [s]")
-ylabel("Kinetic Energy [J]")
-
-save("hybrid_4_KE.mat","t_plt","KE")
-
-%%
+%% PLOT RESUTLS
 f = gen_single_fig;
 load("manual_balance_KE.mat");
 plot(t_plt,KE,'LineWidth',1.5)
-
+disp("Mean KE for manual: " + mean(KE))
 hold on
 
 load("hybrid_4_KE.mat");
@@ -126,3 +150,4 @@ fontsize(13,'points')
 xlabel("Time [s]")
 ylabel("Kinetic Energy [J]")
 legend("Manual Balancing","PID Balancing")
+exportgraphics(f,'C:\Users\camer\OneDrive\Desktop\CZ_Thesis_Latex\plots\hardware_verification_KE.pdf','ContentType','vector');        % axes only
